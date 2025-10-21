abe8dfff88209ecb2f76e3f6493e355c81e0662d
gabrielkfm <sousa.gabriel08@aluno.ifce.edu.br>
2025-10-21 17:16:48 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,7 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Gerente gerente = new Gerente("gabriel", 9000.0, 3000.0);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite o nome do gerente: ");
+        String nome = sc.nextLine();
+        System.out.println("Digite o salário base: ");
+        double salarioBase = sc.nextDouble();
+        sc.nextLine();
+        System.out.println("Digite o salário base: ");
+        double bonus = sc.nextDouble();
+        sc.nextLine();
+
+        Gerente gerente = new Gerente(nome, salarioBase, bonus);
+
         gerente.exibirDados();
+        sc.close();
     }
 }
